cf0c85931730eeb3f63c3f3d3137b59668de9232
Florian König <32238636+koenidv@users.noreply.github.com>
2025-11-25 08:34:04 +0100

    chore: footer

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -14,8 +14,7 @@ export default ((opts?: Options) => {
     return (
       <footer class={`${displayClass ?? ""}`}>
         <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+          Made by <a href="https://koeni.dev">Florian König</a>, based on <a href="https://quartz.jzhao.xyz/">Quartz</a> © {year}
         </p>
         <ul>
           {Object.entries(links).map(([text, link]) => (
